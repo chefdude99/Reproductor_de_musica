9250bc5fe2b5aa27398ccbbbd670112c7a184d71
Chefdude99 <chefdude.1999@gmail.com>
2024-11-13 15:36:06 -0400

    listooo

--- script.js
+++ script.js
@@ -6,13 +6,41 @@ const crearCanción = (informacioncancion) => {
     "https://api.institutoalfa.org/api/songs/image/" +
       informacioncancion.image.filename
   );
-  img.setAttribute('width','238px')
+
+  img.addEventListener("click", () => {
+    document
+      .getElementById("song-front")
+      .setAttribute(
+        "src",
+        "https://api.institutoalfa.org/api/songs/image/" +
+          informacioncancion.image.filename
+      );
+    document
+      .getElementById("song_audio")
+      .setAttribute(
+        "src",
+        "https://api.institutoalfa.org/api/songs/audio/" +
+          informacioncancion.audio.filename
+      );
+  });
+
+  img.setAttribute("width", "238px");
   return img;
 };
 
+document.getElementById("play_boton").addEventListener("click", () => {
+  const audio = document.getElementById("song_audio");
+
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
+
 axios.get("https://api.institutoalfa.org/api/songs").then((res) => {
   let canciones = res.data.songs;
-  canciones = canciones.slice(0,16)
+  canciones = canciones.slice(0, 16);
 
   canciones.map((cancion) => {
     document.getElementById("container").appendChild(crearCanción(cancion));
